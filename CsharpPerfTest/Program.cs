c0cb097abf8f03b73821d3874b5f01b609b186de
Jakub Arnold <darthdeus@gmail.com>
2024-02-16 10:24:07 +0100

    Remove union + "oop"

--- CsharpPerfTest/Program.cs
+++ CsharpPerfTest/Program.cs
@@ -20,7 +20,7 @@ while (true) {
         var vec = new Vector2(Random.Shared.NextSingle() * 20.0f, Random.Shared.NextSingle() * 20.0f);
 
         // var circle = Shape.Circle(vec, Random.Shared.NextSingle() * 2.0f);
-        var circle = Shape.Circle(new CircleShape() { Center = vec, Radius = Random.Shared.NextSingle() * 2.0f });
+        var circle = Shape.Circle(vec, Random.Shared.NextSingle() * 2.0f);
 
         spatial.AddShape(circle, EntityType.Enemy, i);
     }
@@ -36,7 +36,7 @@ while (true) {
     // query 1000 times 
     for (var i = 0; i < 1000; i++) {
         var vec = new Vector2(Random.Shared.NextSingle() * 20.0f, Random.Shared.NextSingle() * 20.0f);
-        var circle = Shape.Circle(new CircleShape() { Center = vec, Radius = 2.0f });
+        var circle = Shape.Circle(vec, 2.0f);
         spatial.Query(result, circle, EntityType.Enemy);
 
         count += result.Count;
--- CsharpPerfTest/SpatialHash.cs
+++ CsharpPerfTest/SpatialHash.cs
@@ -8,119 +8,98 @@ public enum ShapeKind : int {
     Circle,
 }
 
-[StructLayout(LayoutKind.Explicit)]
+// [StructLayout(LayoutKind.Explicit)]
 public struct Shape {
-    [FieldOffset(0)] public ShapeKind kind;
+    public ShapeKind Kind;
+    public Vector2 First;
+    public Vector2 Second;
 
-    [FieldOffset(4)] public AabbShape aabb;
-
-    [FieldOffset(4)] public CircleShape circle;
-
-    public static Shape Circle(CircleShape s) {
-        var shape = new Shape {
-            kind = ShapeKind.Circle,
-            aabb = default,
-            circle = default,
+    public static Shape Circle(Vector2 center, float radius) {
+        return new Shape {
+            Kind = ShapeKind.Circle,
+            First = center,
+            Second = new Vector2(radius),
         };
-        shape.circle = s;
-        return shape;
     }
 
-    public static Shape Aabb(AabbShape s) {
-        var shape = new Shape {
-            kind = ShapeKind.Aabb,
-            aabb = default,
-            circle = default,
+    public Shape BoundingRect() {
+        // return Circle.BoundingRect(First, Second.X);
+
+        return this.Kind switch {
+            ShapeKind.Circle => CircleShape.BoundingRect(this),
+            ShapeKind.Aabb => this,
         };
-        shape.aabb = s;
-        return shape;
-    }
-
-    public AabbShape BoundingRect() {
-        return this.circle.BoundingRect();
-
-        // return this.kind switch {
-        // ShapeKind.Circle => this.circle.BoundingRect(),
-        // ShapeKind.Aabb => this.aabb.BoundingRect(),
-        // };
     }
 
     public readonly bool Intersects(Shape other) {
-        return this.circle.Intersects(other);
+        // return Circle.Intersects(other);
 
-        // return this.kind switch {
-        // ShapeKind.Circle => this.circle.Intersects(other),
-        // ShapeKind.Aabb => this.aabb.Intersects(other),
-        // };
+        return this.Kind switch {
+            ShapeKind.Circle => CircleShape.Intersects(this, other),
+            ShapeKind.Aabb => AabbShape.Intersects(this, other),
+        };
     }
 }
 
 public struct AabbShape {
-    public Vector2 Min;
-    public Vector2 Max;
-
-    public readonly bool IntersectsCircle(CircleShape circle) {
+    public static bool IntersectsCircle(Shape aabb, Shape circle) {
         // NOTE @Perf: Use DistanceSquared to avoid a square root. Yes, it
         // matters.
-        var closest = Vector2.Max(this.Min, Vector2.Min(Max, circle.Center));
-        var distanceSquared = Vector2.DistanceSquared(circle.Center, closest);
-        return distanceSquared <= circle.Radius * circle.Radius;
+        var closest = Vector2.Max(aabb.First, Vector2.Min(aabb.Second, circle.First));
+        var distanceSquared = Vector2.DistanceSquared(circle.First, closest);
+        return distanceSquared <= circle.Second.X * circle.Second.X;
     }
 
-    public readonly bool IntersectsAabb(AabbShape other) {
-        return this.Min.X <= other.Max.X
-               && this.Max.X >= other.Min.X
-               && this.Min.Y <= other.Max.Y
-               && this.Max.Y >= other.Min.Y;
+    public static bool IntersectsAabb(Shape self, Shape other) {
+        return self.First.X <= other.Second.X
+               && self.Second.X >= other.First.X
+               && self.First.Y <= other.Second.Y
+               && self.Second.Y >= other.First.Y;
     }
 
-    public readonly AabbShape BoundingRect() {
-        return this;
-    }
+    public static bool Intersects(Shape self, Shape other) {
+        // return IntersectsCircle(other.circle);
 
-    public readonly bool Intersects(Shape other) {
-        return IntersectsCircle(other.circle);
-
-        // return other.kind switch {
-        // ShapeKind.Circle => this.IntersectsCircle(other.circle),
-        // ShapeKind.Aabb => this.IntersectsAabb(other.aabb),
-        // };
+        return other.Kind switch {
+            ShapeKind.Circle => AabbShape.IntersectsCircle(self, other),
+            ShapeKind.Aabb => AabbShape.IntersectsAabb(self, other),
+        };
     }
 
     public static Shape FromCenterSize(Vector2 position, Vector2 size) {
-        return Shape.Aabb(new AabbShape { Min = position - size / 2, Max = position + size / 2, });
+        return new Shape {
+            Kind = ShapeKind.Aabb,
+            First = position - size / 2, Second = position + size / 2,
+        };
     }
 }
 
 public struct CircleShape {
-    public Vector2 Center;
-    public float Radius;
-
-    public AabbShape BoundingRect() {
-        return new AabbShape {
-            Min = this.Center - new Vector2(this.Radius),
-            Max = this.Center + new Vector2(this.Radius),
+    public static Shape BoundingRect(Shape circle) {
+        return new Shape {
+            First = circle.First - new Vector2(circle.Second.X),
+            Second = circle.First + new Vector2(circle.Second.X),
         };
     }
 
-    public readonly bool IntersectsCircle(CircleShape other) {
-        float r = this.Radius + other.Radius;
-        float distance = Vector2.DistanceSquared(Center, other.Center);
+    public static bool IntersectsCircle(Shape self, Shape other) {
+        float r = self.Second.X + other.Second.X;
+        float distance = Vector2.DistanceSquared(self.First, other.First);
         return distance <= r * r;
     }
 
-    public readonly bool IntersectsAabb(AabbShape aabb) {
-        return aabb.IntersectsCircle(this);
+    public static bool IntersectsAabb(Shape self, Shape aabb) {
+        return AabbShape.IntersectsCircle(self, aabb);
     }
 
-    public readonly bool Intersects(Shape other) {
-        return this.IntersectsCircle(other.circle);
+    public static bool Intersects(Shape self, Shape other) {
+        // return this.IntersectsCircle(other.circle);
 
-        // return other.kind switch {
-        // ShapeKind.Circle => this.IntersectsCircle(other.circle),
-        // ShapeKind.Aabb => this.IntersectsAabb(other.aabb),
-        // _ => throw new NotImplementedException(),
-        // };
+        return other.Kind switch {
+            ShapeKind.Circle => CircleShape.IntersectsCircle(self, other),
+            ShapeKind.Aabb => CircleShape.IntersectsAabb(self, other),
+            // _ => throw new NotImplementedException(),
+        };
     }
 }
 
@@ -201,8 +180,8 @@ public struct SpatialHash {
 
     public void AddShape(Shape shape, EntityType type, int entityId) {
         var rect = shape.BoundingRect();
-        var min = (rect.Min / this.GridSize).Floored();
-        var max = (rect.Max / this.GridSize).Ceiled();
+        var min = (rect.First / this.GridSize).Floored();
+        var max = (rect.Second / this.GridSize).Ceiled();
 
         for (var y = min.Y; y < max.Y; y += 1) {
             for (var x = min.X; x < max.X; x += 1) {
@@ -220,8 +199,8 @@ public struct SpatialHash {
 
     public readonly void Query(HashSet<SpatialHashQueryResult> results, Shape shape, EntityType? filter) {
         var rect = shape.BoundingRect();
-        var min = (rect.Min / this.GridSize).Floored();
-        var max = (rect.Max / this.GridSize).Ceiled();
+        var min = (rect.First / this.GridSize).Floored();
+        var max = (rect.Second / this.GridSize).Ceiled();
 
         for (var y = min.Y; y < max.Y; y += 1) {
             for (var x = min.X; x < max.X; x += 1) {
