a20a2c0f63c6f6734eaf0b2033d4bdde2a929bfe
Jakub Arnold <darthdeus@gmail.com>
2024-02-16 09:23:43 +0100

    Extract utils

---
+++ CsharpPerfTest/Utils.cs
@@ -0,0 +1,69 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace CsharpPerfTest;
+
+public struct Coord {
+    public int X;
+    public int Y;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int MakeKey(float x, float y) {
+        return 1000 * (int)x + (int)y;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Coord FromPos(float x, float y) {
+        int h = SpatialHash.MapSize / 2;
+
+        return new Coord { X = (int)x + h, Y = (int)y + h };
+    }
+}
+
+public class FixedSizeList<T> where T : struct {
+    public T[] Items;
+    public int Count;
+
+    public FixedSizeList(int capacity) {
+        Items = new T[capacity];
+        Count = 0;
+    }
+
+    public ref T this[int index] {
+        get {
+            if (index < 0 || index >= Count) // Now checks _count instead of _items.Length
+                throw new IndexOutOfRangeException();
+            return ref Items[index];
+        }
+    }
+
+    public void Add(T item) {
+        if (Count >= Items.Length) {
+            Grow();
+        }
+
+        Items[Count] = item;
+        Count++;
+    }
+
+    private void Grow() {
+        int newCapacity = Items.Length == 0 ? 4 : Items.Length * 2;
+        T[] newArray = new T[newCapacity];
+        Array.Copy(Items, newArray, Items.Length);
+        Items = newArray;
+    }
+
+    public void Clear() {
+        Count = 0;
+    }
+}
+
+public static class Vec2Extensions {
+    public static Vector2 Floored(this Vector2 v) {
+        return new Vector2((float)Math.Floor(v.X), (float)Math.Floor(v.Y));
+    }
+
+    public static Vector2 Ceiled(this Vector2 v) {
+        return new Vector2((float)Math.Ceiling(v.X), (float)Math.Ceiling(v.Y));
+    }
+}
